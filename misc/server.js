6d763fe3fd836aadb5501434be1acd21b885907c
Kostyantyn Kovalskyy <konstantin@icloak.org>
2015-12-30 21:46:04 +0000

    Finished w3 tutorial.

--- misc/server.js
+++ misc/server.js
@@ -1,4 +1,4 @@
-//Lets require/import the HTTP module
+/*//Lets require/import the HTTP module
 var http = require('http');
 
 //Lets define a port we want to listen to
@@ -8,16 +8,36 @@ const PORT=6969;
 function handleRequest(request, response){
     //response.end('It Works!! Path Hit: ' + request.url);
     //console.log(response);
-    response.json(200, {'test': 'it works!'});
     //response.end("{'kostya':}");
 }
 
 //Create a server
 var server = http.createServer(handleRequest);
 
+
 //Lets start our server
 server.listen(PORT, function(){
    //Callback triggered when server is successfully listening. Hurray!
    console.log("Server listening on: http://localhost:%s", PORT);
 });
+*/
+
+var express = require('express');
+var app = express();
+
+app.get('/assmilk', function(req, res) {
+   res.set('Access-Control-Allow-Origin', '*');
+
+   response = [
+      {'first':'Helen', 'last':'Cabanillas', 'country':'Peru'},
+      {'first':'Kostyantyn', 'last':'Kovalskyy', 'country':'Ukraine'},
+      {'first':'Tony', 'last':'Klimov', 'country':'USA'},
+      {'first':'Ira', 'last':'Klimov', 'country':'Ukraine'}
+
+   ];
+
+   res.send(200, response);
+});
+app.listen(6969);
+
 
