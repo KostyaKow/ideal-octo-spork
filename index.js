f800f50b15074a66751fa52310f0fc5ef7d0aa23
Kostyantyn Kovalskyy <konstantin@icloak.org>
2016-01-02 13:23:17 -0500

    Adding messaging bubbles.

--- index.js
+++ index.js
@@ -16,13 +16,20 @@ var app = angular.module('messanger', []);
 
 app.controller('messages', function($scope) {
 
-   var p1 = new Chatter('bob', '#E96B20');
-   var p2 = new Chatter('john', '#46AEFB');
-
+   var p1 = new Chatter('KungfuNinja', '#E96B20');
+   var p2 = new Chatter('slowCheeta', '#46AEFB');
+   var p3 = new Chatter('MrTurtle', '#FFFF00');
+   var p4 = new Chatter('PhoenixDown', '#00FF00');
+  
    //$scope.setUser(
    $scope.chat = [
-      p1.mkMsg('hello'),
-      p2.mkMsg('blah'),
+      p1.mkMsg('Sup dude'),
+      p2.mkMsg('Yo, what it do homie'),
+      p1.mkMsg('nm just swagging you know how it b dawg'),
+      p2.mkMsg('werd'),
+      p1.mkMsg('tru'),
+      p3.mkMsg('Kawabunga'),
+      p4.mkMsg('Sup bros')
    ];
 
 });
